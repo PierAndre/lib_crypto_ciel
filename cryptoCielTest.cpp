3d4401e03d4288f267ad61bb994a41a99c1c8f43
pv <test@pv.com>
2024-09-24 17:49:30 +0200

    chiffrement d'un fichier en RSA

--- cryptoCielTest.cpp
+++ cryptoCielTest.cpp
@@ -14,22 +14,29 @@ int main()
 	std::cout << LM.CalculateSHA256(monMessageHash) << std::endl;
 	*/
 
-	
+	/*
 	AesGestion cryptAES;
 	cryptAES.GenerateAESKey();
 	std::string monFichierCle = "maCleSecrete.bin";
-	std::string clairMessage = "message_secret.txt";
-	std::string chiffreMessage = "message_chiffre.txt";
-	std::string dechiffreMessage = "message_dechiffre.txt";
+	std::string clairMessage = "message_secret_AES.txt";
+	std::string chiffreMessage = "message_chiffre_AES.txt";
+	std::string dechiffreMessage = "message_dechiffre_AES.txt";
 	//cryptAES.SaveAESKeyToFile(monFichierCle);
 	cryptAES.LoadAESKeyFromFile(monFichierCle);
 	cryptAES.EncryptFileAES256(clairMessage, chiffreMessage);
 	cryptAES.DecryptFileAES256(chiffreMessage, dechiffreMessage);
-	
+	*/
 
 
-	//RsaGestion cryptRSA;
-	//cryptRSA.generationClef(PApublic,PAprivée);
+	RsaGestion cryptRSA;
+	std::string PApublic = "PAClePublic.PEM";
+	std::string PAprive = "PAClePrive.PEM";
+	std::string message_secret_RSA = "message_secret_RSA.txt";
+	std::string message_chiffre_RSA = "message_chiffre_RSA.txt";
+	cryptRSA.generationClef(PApublic, PAprive,1024);
+	cryptRSA.chiffrementFichier(message_secret_RSA, message_chiffre_RSA, true);
+	//cryptRSA.dechiffrementRsa();
+
 
 }
 
